92fd77755abc0e6993e2f98c8f81421f2f59ba1f
HexisHakon <spammailspam1234@gmx.de>
2025-02-24 14:11:18 +0100

    updated to english language

--- client/src/components/VoiceHistogram.vue
+++ client/src/components/VoiceHistogram.vue
@@ -1,5 +1,9 @@
 <template>
     <div id="histogram-wrapper">
+        <div id="button-wrapper">
+            <button @click="createHistogram()"><img src="../assets/Chart.webp"></button>
+            <button @click="renderRelativeChart()"><img src="../assets/ChartPercentage.webp"></button>
+        </div>
         <div id="histogram-container">
             <svg id="d3-histogram-container">
             </svg>
@@ -67,12 +71,13 @@ export default {
           .domain(Array.from(Array(Math.ceil((this.$store.state.matchData.gameDuration / 60.0)*6) + 1).keys()))
           .range([1,width])
 
-          let maxYValue = 120
+          let maxYValue = /* 120 */ Math.max(...data.map(x => parseFloat(x.p1 + x.p2 + x.p3 + x.p4 + x.p5 + x.p6 + x.p7 + x.p8 + x.p9 + x.p10)))
           const yScale = d3
           .scaleLinear()
           .domain([0,maxYValue])
           .range([height, 0])
 
+          document.querySelector('#d3-histogram-container').innerHTML = ""
           const container = d3.select('#d3-histogram-container')
           .classed('chart-container', true)
           .style('height', `${height}px`)
@@ -102,8 +107,8 @@ export default {
             .enter().append("rect")
                 .attr("x", d => {return xScale(d.data.x)})
                 .attr("y", d => {return yScale(d[1])})
-                .attr("height", d => {return yScale(d[0]) - yScale(d[1])})
-                .attr("width", xScale.bandwidth())
+                .attr("height", d => {return yScale(d[0]) - yScale(d[1]) - 1})
+                .attr("width", xScale.bandwidth() - 1)
 
           container.append("g")
           .attr("transform","translate(0," + height + ")")
@@ -112,7 +117,7 @@ export default {
 
             if(this.histogramDataBlue.concat(this.histogramDataRed).length>0){
                 container.append("g")
-                .call(d3.axisLeft(yScale).tickValues([0,5,10,15,20,25,30,35,40,45,50,55,60,65,70,75,80,85,90,95,100,105,110,115,120]))
+                .call(d3.axisLeft(yScale).tickValues(Array.from({length: Math.ceil(maxYValue/5)}, (e,i) => i*5)))
                 .selectAll('text')
             } else {
                 container.append("g")
@@ -136,6 +141,120 @@ export default {
           .attr("fill", "white")
           .attr("font-size", "10px")
           .text("Word count")
+      },
+      renderRelativeChart() {
+          let data = Array.from(Array(Math.ceil((this.$store.state.matchData.gameDuration / 60.0)*6) + 1).keys()).map(x => {
+            let p1Count = this.histogramDataBlue[0][x] ? this.histogramDataBlue[0][x].y : 0
+            let p2Count = this.histogramDataBlue[1][x] ? this.histogramDataBlue[1][x].y : 0
+            let p3Count = this.histogramDataBlue[2][x] ? this.histogramDataBlue[2][x].y : 0
+            let p4Count = this.histogramDataBlue[3][x] ? this.histogramDataBlue[3][x].y : 0
+            let p5Count = this.histogramDataBlue[4][x] ? this.histogramDataBlue[4][x].y : 0
+            let p6Count = this.histogramDataRed[0][x] ?  this.histogramDataRed[0][x].y : 0
+            let p7Count = this.histogramDataRed[1][x] ?  this.histogramDataRed[1][x].y : 0
+            let p8Count = this.histogramDataRed[2][x] ?  this.histogramDataRed[2][x].y : 0
+            let p9Count = this.histogramDataRed[3][x] ?  this.histogramDataRed[3][x].y : 0
+            let p10Count = this.histogramDataRed[4][x] ?  this.histogramDataRed[4][x].y : 0
+            let dataPoint = {
+                x: x,
+                p1: p1Count,
+                p2: p2Count,
+                p3: p3Count,
+                p4: p4Count,
+                p5: p5Count,
+                p6: p6Count,
+                p7: p7Count,
+                p8: p8Count,
+                p9: p9Count,
+                p10: p10Count,
+            max: p1Count + p2Count + p3Count + p4Count + p5Count + p6Count + p7Count + p8Count + p9Count + p10Count}
+              return dataPoint
+          })
+
+          let subgroups = [];
+          for(let value in this.selectedPlayers){
+              if(!this.selectedPlayers[value]) continue
+              subgroups.push(`p${parseInt(value) + 1}`)
+          }
+
+          let stackedData = d3.stack().keys(subgroups)(data)
+
+          const height = document.getElementById('histogram-container').getBoundingClientRect().height;
+          const width = document.getElementById('histogram-container').getBoundingClientRect().width;
+          const xScale = d3
+          .scaleBand()
+          .domain(Array.from(Array(Math.ceil((this.$store.state.matchData.gameDuration / 60.0)*6) + 1).keys()))
+          .range([1,width])
+
+          let maxYValue = 100
+          const yScale = d3
+          .scaleLinear()
+          .domain([0,maxYValue])
+          .range([height, 0])
+
+          document.querySelector('#d3-histogram-container').innerHTML = ""
+          const container = d3.select('#d3-histogram-container')
+          .classed('chart-container', true)
+          .style('height', `${height}px`)
+          .style('width', `${width}px`)
+
+            let fillRange = [];
+            for(let index in this.histogramDataBlue){
+                if(this.histogramDataBlue[index].length == 0) continue
+                fillRange.push('#3790e1')
+            }
+            for(let index in this.histogramDataRed){
+                if(this.histogramDataRed[index].length == 0) continue
+                fillRange.push('#e13939')
+            }
+
+          let fill = d3.scaleOrdinal()
+            .domain(subgroups)
+            .range(fillRange)
+
+          container.append("g")
+          .selectAll("g")
+          .data(stackedData)
+          .enter().append("g")
+            .attr("fill", d => {return fill(d)})
+            .selectAll("rect")
+            .data(d => {return d})
+            .enter().append("rect")
+                .attr("x", d => {return xScale(d.data.x)})
+                .attr("y", d => {return yScale((d[1]/d.data.max)*maxYValue)})
+                .attr("height", d => {return yScale((d[0]/d.data.max)*maxYValue) - yScale((d[1]/d.data.max)*maxYValue) - 1})
+                .attr("width", xScale.bandwidth() - 1)
+
+          container.append("g")
+          .attr("transform","translate(0," + height + ")")
+          .call(d3.axisBottom(xScale).tickFormat(function(d,i){return i>0 && i %30 === 0 ? `${d/6} min` : null}))
+          .selectAll('text')
+
+            if(this.histogramDataBlue.concat(this.histogramDataRed).length>0){
+                container.append("g")
+                .call(d3.axisLeft(yScale).tickValues(Array.from({length: Math.ceil(maxYValue/5) + 1}, (e,i) => i*5)))
+                .selectAll('text')
+            } else {
+                container.append("g")
+                .call(d3.axisLeft(yScale))
+                .selectAll('text')
+            }
+
+
+          container.append("text")
+          .attr("text-anchor", "end")
+          .attr("x", width)
+          .attr("y", height + 35)
+          .attr("fill", "white")
+          .attr("font-size", "10px")
+          .text("Time in 10s intervals")
+
+          container.append("text")
+          .attr("text-anchor", "middle")
+          .attr("x", 10)
+          .attr("y", -10)
+          .attr("fill", "white")
+          .attr("font-size", "10px")
+          .text("Word count percentage")
       },
       selectStartingPoint() {
         this.$store.commit('updateSelectBarPressed')
@@ -165,15 +284,17 @@ export default {
             this.$store.commit('updateSelectedIntervalStart',{bar: ''})
             this.$store.commit('updateSelectedIntervalLength', {length: 0})
         }
+      },
+      createHistogram(){
+        this.fillHistogramBlueRed()
+        this.renderChart()
       }
   },
   mounted() {
-    this.fillHistogramBlueRed()
-    this.renderChart()
+    this.createHistogram()
   },
   updated() {
-    this.fillHistogramBlueRed()
-    this.renderChart()
+    this.createHistogram()
   },
   beforeUpdate() {
     var svg = d3.select("svg")
@@ -185,6 +306,7 @@ export default {
 
 <style scoped>
 #histogram-wrapper {
+    position: relative;
     display: flex;
     flex-direction: column;
     justify-content: center;
@@ -205,6 +327,44 @@ export default {
     justify-content: left;
     margin-top: 30px;
     gap: 10px;
+}
+#button-wrapper {
+    position: absolute;
+    display: flex;
+    flex-direction: column;
+    gap: 5px;
+    top: 0;
+    left: 0;
+    height: 100%;
+    width: 50px;
+    padding: 5px;
+
+    & > button {
+        display: flex;
+        flex: 1;
+        width: 100%;
+        justify-content: center;
+        align-items: center;
+        border: none;
+        border-radius: 5px;
+        background-color: rgb(var(--EventGraphBG));
+        filter: brightness(110%);
+        box-shadow: 0 3px 3px 0 #0006;
+        cursor: pointer;
+
+        &:hover {
+            background-color: rgba(var(--EventGraphBG),0.8);
+        }
+
+        &:active {
+            background-color: rgba(var(--EventGraphBG),0.2);
+        }
+
+        & > img {
+            width: 80%;
+            aspect-ratio: 1;
+        }
+    }
 }
 .button {
     background-color: rgb(var(--ActiveSidebarEffect));
--- server/routes/api/stopwords.js
+++ server/routes/api/stopwords.js
@@ -4,7 +4,7 @@ const path = require('path');
 
 const router = Router();
 const recordingPath = path.normalize(__dirname + "../../../assets");
-const stopWordsArray = fs.readFileSync(recordingPath + "/stop_words_german.json")
+const stopWordsArray = fs.readFileSync(recordingPath + "/stop_words_english.json")
 
 router.get('/',(req, res) => {
     res.status(200).send(JSON.parse(stopWordsArray))
